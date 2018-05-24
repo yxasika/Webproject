5432e66a4fd4ba6cd1d9ab718a68a160281e5243
NvHungDao <38260010+NvHungDao@users.noreply.github.com>
2018-05-24 17:15:09 +0200

    Endlich!!!!!!!!!Hab das Popup für das Login hinbekommen! Wuhu

---
+++ scripts/loginPopup.js
@@ -0,0 +1,8 @@
+$(function () {
+var modal = document.getElementById('modal-wrapper');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+});
