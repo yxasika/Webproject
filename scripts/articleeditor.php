ded9792494422f6b4f6a0aff056b86babfa895ee
Lukas <l-sander@web.de>
2018-06-08 10:58:31 +0200

    syrfvtnlweirgnbseoirugbn

---
+++ scripts/articleeditor.php
@@ -0,0 +1,17 @@
+<?php
+$source = file_get_contents("../json/articles.json");
+$articles = json_decode($source, true);
+
+foreach ($articles as $article => $article)
+{
+
+}
+
+if(isset($POST["approve"]))
+{
+
+
+
+}
+
+?>
