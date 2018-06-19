18fbcc40f3f33ad03d1a8816441cc60292f4a898
NvHungDao <38260010+NvHungDao@users.noreply.github.com>
2018-06-19 09:39:04 +0200

    notification tabs funktionieren theoretisch, nur wird der Inhalt nicht aktuallisiert

--- views/notification.php
+++ views/notification.php
@@ -39,38 +39,53 @@ session_start();
     <div class="container">
         <div class="row justify-content-center">
             <div class="content">
-                <ul class="nav nav-tabs">
+                <ul class="nav nav-tabs" id="pills-tab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">New Messages</a>
+                        <a class="nav-link active" id="pills-newMessage-tab" data-toggle="pill" href="#newMessage"
+                        role="tab" aria-controls="pills-newMessage" aria-selected="true">New Messages</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Read Messages</a>
+                        <a class="nav-link" id="pills-readMessage-tab" data-toggle="pill" href="#readMessage"
+                           role="tab" aria-controls="pills-readMessage" aria-selected="false">Read Messages</a>
                     </li>
                 </ul>
                 <br>
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <?php
-                            $notifi = getNotifi();
-                            foreach ($notifi as $notification => $notification) {
-                                if ($notifi[$notification]['receiver'] == ($_SESSION['email'])) {
-                                    print '<div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Subject: ' . htmlspecialchars($notifi[$notification]['subject']) . '</h5>
-                                    <p class="text-muted">' . htmlspecialchars($notifi[$notification]['sender']) . '</p>
-                                    <h6 class="card-title">Message:</h6>
-                                    <p class="card-text">' . htmlspecialchars($notifi[$notification]['message']) . '</p>
-                                    <a class="btn btn-primary">Answer</a>
-                                    <a class="btn btn-secondary">Marked read</a>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-newMessage" role="tabpanel"
+                         aria-labelledby="pills-newMessage-tab">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <?php
+                                    $notifi = getNotifi();
+                                    foreach ($notifi as $notification => $notification) {
+                                        if ($notifi[$notification]['receiver'] == ($_SESSION['email'])) {
+                                            print '<div class="card">
+                                                    <div class="card-body">
+                                                    <h5 class="card-title">Subject: ' . htmlspecialchars($notifi[$notification]['subject']) . '</h5>
+                                                    <p class="text-muted">' . htmlspecialchars($notifi[$notification]['sender']) . '</p>
+                                                    <h6 class="card-title">Message:</h6>
+                                                    <p class="card-text">' . htmlspecialchars($notifi[$notification]['message']) . '</p>
+                                                    <a class="btn btn-primary">Answer</a>
+                                                    <a class="btn btn-secondary">Marked read</a>
+                                                    </div>
+                                                    </div>';
+                                        }
+                                    }
+                                    ?>
                                 </div>
-                            </div>';
-                                }
-                            }
-                            ?>
+                            </div>
                         </div>
+                    </div>
+                    <div class="tab-pane fade show active" id="pills-newMessage" role="tabpanel"
+                         aria-labelledby="pills-readMessage-tab">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
 
-
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
