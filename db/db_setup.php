a03fb412b85428f84c1f894e63037fb69f33610a
Lukas <l-sander@web.de>
2018-06-21 15:33:42 +0200

    published date wird nun bei den neusten artikeln angezeigt

--- db/db_setup.php
+++ db/db_setup.php
@@ -9,12 +9,14 @@ $userObj->insertUser('test', 'Mustertest', 'test@mail.com', md5("test"), 'author
 $userObj->insertUser('Max', 'Musterman', 'muster@mail.com', md5("muster"), 'editor');
 
 $articleObj = new article();
-$articleObj->insertArticle('testArticle', 'test Mustertest', 'test@mail.com' ,  '2018-06-15', 2, NULL, NULL, 'pending', 'blablabla', '4');
+$articleObj->insertArticle('testArticle', 'test Mustertest', 'test@mail.com' ,  '2018-06-15', 2, NULL, NULL, 'published', 'blablabla', '1');
+$articleObj->insertArticle('testArticle3', 'test Mustertest', 'test@mail.com' ,  '2018-06-10', 3, NULL, NULL, 'published', 'blablabla', '3');
 $articleObj->insertArticle('testArticle2', 'Max Mustermann', 'muster@mail.com' ,  '2018-06-15', 1, NULL, NULL, 'pending', 'blablabla', '4');
 
 $categoryObj = new category();
 $categoryObj->insertCategory(1, 'rpg');
 $categoryObj->insertCategory(2, 'shooter');
+$categoryObj->insertCategory(3, 'rpg');
 
 $notifiObj = new notification();
 $notifiObj->insertNotifi('test@mail.com', 'muster@mail.com', 'Missing Abstract',  'Here is a text message.', 'new');
