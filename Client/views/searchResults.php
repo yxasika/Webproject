98c3eeb9fc8c1c108283daa5accd8a128f15a272
NvHungDao <38260010+NvHungDao@users.noreply.github.com>
2018-07-02 22:49:52 +0200

    Navigationsbar mit jquery und lesebaren Code

--- Client/views/searchResults.php
+++ Client/views/searchResults.php
@@ -29,65 +29,55 @@ session_start();
     <script rel="script" type="text/javascript" src="../scripts/jquery.rotate.1-1.js"></script>
     <script type="text/javascript" src="../scripts/search.js"></script>
     <link rel="stylesheet" type="text/css" href="../css/search.css">
-    <link rel="stylesheet" type="text/css" href="../css/searchBar.css">
-
 </head>
 <body>
 
-<?php include "navbar.php" ?>
+<?php include "blackNav.php" ?>
 
-<main>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-6">
-                <ul class="searchNav">
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=A">A</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=B">B</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=C">C</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=D">D</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=E">E</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=F">F</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=G">G</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=H">H</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=I">I</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=J">J</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=K">K</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=L">L</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=M">M</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=N">N</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=O">O</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=P">P</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=Q">Q</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=R">R</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=S">S</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=T">T</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=U">U</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=V">V</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=W">W</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=X">X</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=Y">Y</a></li>
-                    <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=Z">Z</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-6">
-                <form method="get" action="">
-                    <input class="searchInput form-control-static" name="search">
-                    <button type="submit"><i class="material-icons btn btn-primary btn-sm">search</i></button>
-                </form>
-            </div>
-        </div>
-        <hr>
-        <div class="row justify-content-center">
-            <div class="content">
-                <ul>
-                    <?php include "../scripts/search.php" ?>
-                </ul>
-            </div>
+<section class="contentS">
+    <ul class="searchNav">
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=A">A</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=B">B</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=C">C</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=D">D</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=E">E</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=F">F</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=G">G</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=H">H</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=I">I</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=J">J</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=K">K</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=L">L</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=M">M</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=N">N</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=O">O</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=P">P</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=Q">Q</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=R">R</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=S">S</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=T">T</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=U">U</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=V">V</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=W">W</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=X">X</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=Y">Y</a></li>
+        <li><a href="http://localhost/Webproject/Client/views/searchResults.php?letter=Z">Z</a></li>
+    </ul>
+    <div>
+        <form method="get" action="">
+            <input class="searchInput" name="search">
+            <button type="submit"><i class="material-icons">search</i></button>
+        </form>
+    </div>
+    <hr>
+    <div class="row justify-content-center">
+        <div class="content">
+            <ul>
+                <?php include "../scripts/search.php" ?>
+            </ul>
         </div>
     </div>
-</main>
+</section>
 
 <?php include "footer.php" ?>
 
